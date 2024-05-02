45fbc66779abe80cecd2df489ecf6d4cbf59afa3
yudha490 <yudhapratama8944@gmail.com>
2024-05-02 11:03:51 +0700

    menabahkan API mahasiswa

--- modul10_1302223117/Controllers/mahasiswacontroller.cs
+++ modul10_1302223117/Controllers/mahasiswacontroller.cs
@@ -1,6 +1,23 @@
-﻿namespace modul10_1302223117.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace modul10_1302223117.Controllers
 {
-    public class mahasiswacontroller
+    [ApiController]
+    [Route("[controller]")]
+    public class mahasiswacontroller : ControllerBase
     {
+        private readonly ILogger<mahasiswacontroller> _logger;
+
+        public mahasiswacontroller(ILogger<mahasiswacontroller> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetMahasiswa")]
+
+        public IEnumerable<mahasiswa> Get()
+        { return Enumerable.Empty<mahasiswa>();
+        
+        }
     }
 }
--- modul10_1302223117/mahasiswa.cs
+++ modul10_1302223117/mahasiswa.cs
@@ -1,6 +1,19 @@
-﻿namespace modul10_1302223117
+﻿using Microsoft.VisualBasic;
+
+namespace modul10_1302223117
 {
     public class mahasiswa
     {
+        public string name { get; set; }
+        public string nim {  get; set; }
+
+        public List<string> course;
+
+        public int year { get; set; }
+
+        public mahasiswa()
+        {
+
+        }
     }
 }
